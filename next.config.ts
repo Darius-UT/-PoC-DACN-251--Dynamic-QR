30f45e50cab3498d44a6e43beed28b82e6bb8b40
Darius-UT <nguyenlehoangphuc707@gmail.com>
2026-01-04 01:41:51 +0700

    Add PWA config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",           // Nơi lưu các file service worker
+  cacheOnFrontEndNav: true, // Cache khi chuyển trang
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,     // Tự load lại khi có mạng
+  disable: false,           // Để false để test PWA (hoặc để process.env.NODE_ENV === 'development' nếu k muốn cache lúc code)
+  workboxOptions: {
+    disableDevLogs: true,   // Tắt log đỡ rối mắt
+  },
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
